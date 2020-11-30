dcef2703a754e73810359d56cc1ea0711dedb184
crangoren <crangoren@gmail.com>
2020-11-30 20:38:48 +0300

    Initial commit

---
+++ src/homework5/employee.java
@@ -0,0 +1,12 @@
+package homework5;
+
+public class employee {
+
+    String firstLastName;
+    String position;
+    String emailAddress;
+    String phoneNumber;
+    int salary;
+    int age;
+
+}
